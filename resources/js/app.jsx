5f8bb7c5bed99499eef83aa8402047dcb1d4c7df
Max <39443507+kennethlorrell@users.noreply.github.com>
2023-11-12 20:30:14 +0200

    Initialize new Laravel project

---
+++ resources/js/app.jsx
@@ -0,0 +1,8 @@
+import './bootstrap';
+import { render } from 'react-dom';
+import ProductsIndex from 'resources/js/Pages/Posts/ProductsIndex.js';
+
+render(
+  <ProductsIndex />,
+  document.getElementById('app')
+);
